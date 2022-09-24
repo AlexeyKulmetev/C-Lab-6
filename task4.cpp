1e9e2bc70a4ef8f0469df1fbb940939abdd6f895
AlexeyKulmetev <44574285+AlexeyKulmetev@users.noreply.github.com>
2022-09-24 15:33:47 +0300

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -1,9 +1,7 @@
 #include "task4.h"
 
 long long sumC(char* arr, int len) {
-
 	long long sum = 0;
-
 	for (int i = 0; i < len; i++) {
 		sum += arr[i] - 48;
 	}
@@ -12,11 +10,10 @@ long long sumC(char* arr, int len) {
 
 
 long long sumR(char* arr, int len) {
-
 	if (len == 1) {
 		return *arr - 48;
 	}
 	else {
-		return (* arr - 48) + sumR(arr + 1, len - 1);
-	}		
+		return (*arr - 48) + sumR(arr + 1, len - 1);
+	}
 }
